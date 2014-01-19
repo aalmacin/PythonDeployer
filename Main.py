fedef3858588fbf6cc3790fdcffbec6ade33d4d4
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2014-01-18 20:54:02 -0500

    Fixed widget positioning

--- Main.py
+++ Main.py
@@ -22,32 +22,34 @@ class Application(Frame):
 
     def createWidgets(self):
       # Home Button
-      self.home_button = Button(text=Constants.HOME_TXT, command=self.show_home)
-      self.home_button.grid(row=0, column=0, ipadx=Constants.C_PADDING_SIZE, ipady=Constants.C_PADDING_SIZE)
+      self.button_frame = Frame(self)
+      self.button_frame.home_button = Button(text=Constants.HOME_TXT, command=self.show_home)
+      self.button_frame.home_button.grid(row=0, column=0, ipadx=Constants.C_PADDING_SIZE, ipady=Constants.C_PADDING_SIZE)
 
-      self.project_button = Button(text=Constants.PROJECT_TXT, command=self.show_project)
-      self.project_button.grid(row=0, column=1, ipadx=Constants.C_PADDING_SIZE, ipady=Constants.C_PADDING_SIZE)
+      self.button_frame.project_button = Button(text=Constants.PROJECT_TXT, command=self.show_project)
+      self.button_frame.project_button.grid(row=0, column=1, ipadx=Constants.C_PADDING_SIZE, ipady=Constants.C_PADDING_SIZE)
 
-      self.deploy_button = Button(text=Constants.DEPLOY_TXT, command=self.show_deploy)
-      self.deploy_button.grid(row=0, column=2, ipadx=Constants.C_PADDING_SIZE, ipady=Constants.C_PADDING_SIZE)
+      self.button_frame.deploy_button = Button(text=Constants.DEPLOY_TXT, command=self.show_deploy)
+      self.button_frame.deploy_button.grid(row=0, column=2, ipadx=Constants.C_PADDING_SIZE, ipady=Constants.C_PADDING_SIZE)
+      self.button_frame.grid(row=0, column=0, columnspan=3)
 
       self.quit_button = Button(text='Quit', command=self.quit)
-      self.quit_button.grid(row=2, column=0, ipadx=Constants.C_PADDING_SIZE, ipady=Constants.C_PADDING_SIZE)
+      self.quit_button.grid(row=2, column=0, columnspan=3, ipadx=Constants.C_PADDING_SIZE, ipady=Constants.C_PADDING_SIZE)
 
       self.home = Home()
-      self.home.grid(row=1, column=0)
+      self.home.grid(row=1, column=0, columnspan=3)
 
       self.projects = Project()
-      self.projects.grid(row=1, column=0)
+      self.projects.grid(row=1, column=0, columnspan=3)
       self.projects.grid_remove()
 
       self.deployer = Deploy()
-      self.deployer.grid(row=1, column=0)
+      self.deployer.grid(row=1, column=0, columnspan=3)
       self.deployer.grid_remove()
 
 
     def __init__(self, master=None):
-        Frame.__init__(self, master)
+        Frame.__init__(self, master, width=Constants.APP_WIDTH, height=Constants.APP_HEIGHT)
         self.grid()
         self.createWidgets()
 
